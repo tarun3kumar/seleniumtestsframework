719edf6619e4229d51ccb212a4ff45442fe0ee5d
tbhadauria <tbhadauria@paypal.com>
2013-10-02 16:58:29 +0530

    add comment

--- src/test/java/com/seleniumtests/webtests/RetryTest1.java
+++ src/test/java/com/seleniumtests/webtests/RetryTest1.java
@@ -6,7 +6,10 @@ import com.seleniumtests.controller.TestPlan;
 import org.testng.annotations.Test;
 
 public class RetryTest1 extends TestPlan{
-	
+
+    /**
+     * Retires thrice as test fails
+     */
 	@Test(groups="retryTest1")
 	public void retryFailedTest() {
 		assert 1==2:"bound to fail";
