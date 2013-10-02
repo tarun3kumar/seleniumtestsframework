9bc08b3eb2e112a033cb7b03d38f014c100f7997
tbhadauria <tbhadauria@paypal.com>
2013-10-02 17:09:39 +0530

    add soft assertion test

---
+++ src/test/java/com/seleniumtests/tests/SoftAssertionTest.java
@@ -0,0 +1,23 @@
+package com.seleniumtests.tests;
+
+import static com.seleniumtests.controller.Assertion.*;
+import org.testng.annotations.Test;
+
+/**
+ * User: tbhadauria
+ * Date: 10/2/13
+ * Time: 4:59 PM
+ */
+public class SoftAssertionTest {
+
+    /**
+     * Continues with test execution even though assertions fail
+     */
+    @Test(groups = "softAssertionTest", description = "Continues with test execution even though assertions fail")
+    public void softAssertionTest() {
+        assertEquals(true, false, "boolean test failure");
+        assertTrue(false, "another boolean test failure");
+        assertEquals("selenium", "qtp", "universal test failure :)");
+        assertTrue(1==1, "never fails");
+    }
+}
