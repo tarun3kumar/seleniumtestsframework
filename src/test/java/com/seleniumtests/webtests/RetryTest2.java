0a9c97a6fa13b70571aa981994aa8127f5017085
tbhadauria <tbhadauria@paypal.com>
2013-10-02 16:58:57 +0530

    add comment

--- src/test/java/com/seleniumtests/webtests/RetryTest2.java
+++ src/test/java/com/seleniumtests/webtests/RetryTest2.java
@@ -6,7 +6,10 @@ import org.testng.annotations.Test;
 import java.util.GregorianCalendar;
 
 public class RetryTest2 extends TestPlan {
-	
+
+    /**
+     * Will not retry as test would not fail
+     */
 	@Test(groups="retryTest2")
 	public void retryFailedTest() {
 		assert 1==1:"always pass";
