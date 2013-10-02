ba8a14aa1e1d0588685263828a4f000549f27270
tbhadauria <tbhadauria@paypal.com>
2013-10-02 17:08:32 +0530

    add description

--- src/test/java/com/seleniumtests/tests/RetryTest1.java
+++ src/test/java/com/seleniumtests/tests/RetryTest1.java
@@ -1,6 +1,4 @@
-package com.seleniumtests.webtests;
-
-import java.util.GregorianCalendar;
+package com.seleniumtests.tests;
 
 import com.seleniumtests.controller.TestPlan;
 import org.testng.annotations.Test;
@@ -8,9 +6,9 @@ import org.testng.annotations.Test;
 public class RetryTest1 extends TestPlan{
 
     /**
-     * Retires thrice as test fails
+     * Retires thrice as test is bound to fail
      */
-	@Test(groups="retryTest1")
+	@Test(groups="retryTest1", description = "Retires thrice as test is bound to fail")
 	public void retryFailedTest() {
 		assert 1==2:"bound to fail";
 	}
