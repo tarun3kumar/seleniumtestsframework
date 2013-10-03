a5c18a130a6b9bd1771abe3d188cdb82bfd7556b
tbhadauria <tbhadauria@paypal.com>
2013-10-03 12:32:05 +0530

    update file name

--- src/main/java/com/seleniumtests/reporter/HTMLReporter.java
+++ src/main/java/com/seleniumtests/reporter/HTMLReporter.java
@@ -245,7 +245,7 @@ public class HTMLReporter implements IReporter, ITestListener,IInvokedMethodList
 		resources.add("reporter" + File.separator + "images" + File.separator + "mktree" + File.separator + "test2.gif");
 		resources.add("reporter" + File.separator + "images" + File.separator + "mktree" + File.separator + "test3.gif");
 		resources.add("reporter" + File.separator + "images" + File.separator + "mktree" + File.separator + "test3.gif");
-		resources.add("reporter" + File.separator + "images" + File.separator + "yukontoolbox" + File.separator + "footerbar_grad.gif");
+		resources.add("reporter" + File.separator + "images" + File.separator + "yukontoolbox" + File.separator + "seleniumtests_footer_grad.gif");
 		resources.add("reporter" + File.separator + "images" + File.separator + "yukontoolbox" + File.separator + "seleniumtests_mid.gif");
 		resources.add("reporter" + File.separator + "images" + File.separator + "yukontoolbox" + File.separator + "grey_bl.gif");
 		resources.add("reporter" + File.separator + "images" + File.separator + "yukontoolbox" + File.separator + "grey_br.gif");
