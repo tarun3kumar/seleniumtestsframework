228cb3398a61e12f2d9e5dec4555dd3593b8b4e6
tbhadauria <tbhadauria@paypal.com>
2013-10-14 12:43:36 +0530

    remove unused code

--- src/main/java/com/seleniumtests/controller/DefaultTestNGContext.java
+++ src/main/java/com/seleniumtests/controller/DefaultTestNGContext.java
@@ -16,89 +16,75 @@ import com.google.inject.Module;
 
 public class DefaultTestNGContext implements ITestContext {
 
-	private static final long serialVersionUID = 3809658526152767666L;
+	private static final long serialVersionUID = 2710769637263878789L;
 	ISuite suite;
 
 	
 	public DefaultTestNGContext() {
-		this.suite = new DefaultSuite();
+		this.suite = new SeleniumTestsDefaultSuite();
 	}
 
 
 	public Object getAttribute(String name) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	
 	public void setAttribute(String name, Object value) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	
 	public Set<String> getAttributeNames() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	
 	public Object removeAttribute(String name) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	
 	public String getName() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	
 	public Date getStartDate() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	
 	public Date getEndDate() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	
 	public IResultMap getPassedTests() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	
 	public IResultMap getSkippedTests() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	
 	public IResultMap getFailedButWithinSuccessPercentageTests() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	
 	public IResultMap getFailedTests() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	
 	public String[] getIncludedGroups() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	
 	public String[] getExcludedGroups() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
@@ -115,68 +101,55 @@ public class DefaultTestNGContext implements ITestContext {
 
 	
 	public ITestNGMethod[] getAllTestMethods() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	
 	public String getHost() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	
 	public Collection<ITestNGMethod> getExcludedMethods() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	
 	public IResultMap getPassedConfigurations() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	
 	public IResultMap getSkippedConfigurations() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	
 	public IResultMap getFailedConfigurations() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	
 	public XmlTest getCurrentXmlTest() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	
 	public List<Module> getGuiceModules(Class<? extends Module> cls) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	
 	public void addGuiceModule(Class<? extends Module> cls, Module module) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	
 	public Injector getInjector(List<Module> moduleInstances) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	
 	public void addInjector(List<Module> moduleInstances, Injector injector) {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
