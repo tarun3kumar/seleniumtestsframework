f8a3d3873816e003f8690adc2552bcfe78367edf
tbhadauria <tbhadauria@paypal.com>
2013-10-14 12:41:22 +0530

    remove unused code

---
+++ src/main/java/com/seleniumtests/controller/SeleniumTestsPageListener.java
@@ -0,0 +1,58 @@
+package com.seleniumtests.controller;
+
+import org.apache.log4j.Logger;
+
+import com.seleniumtests.driver.web.element.IPage;
+import com.seleniumtests.helper.URLAssistant;
+import com.seleniumtests.reporter.PluginsUtil;
+
+/**
+ * Plugin architecture for SEO, Java Script, Accessibility etc tests.
+ */
+public abstract class SeleniumTestsPageListener {
+    protected static final Logger logger = Logging.getLogger(SeleniumTestsPageListener.class);
+
+    /**
+     * Informs all the page listeners on page Load
+     *
+     * @param page
+     */
+    public static void informPageLoad(IPage page) {
+        PluginsUtil.getInstance().invokePageListeners(SeleniumTestsContextManager.getThreadContext().getTestMethodSignature(),
+                page, true);
+    }
+
+    /**
+     * Informs all the page listeners on page Unload
+     *
+     * @param page
+     */
+    public static void informPageUnload(IPage page) {
+        PluginsUtil.getInstance().invokePageListeners(SeleniumTestsContextManager.getThreadContext().getTestMethodSignature(),
+                page, false);
+    }
+
+    private String title;
+
+    private boolean testResultEffected;
+
+    public SeleniumTestsPageListener(String title, boolean testResultEffected) {
+        this.title = title;
+        this.testResultEffected = testResultEffected;
+    }
+
+    public SeleniumTestsPageListener() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public boolean isTestResultEffected() {
+        return testResultEffected;
+    }
+
+    public abstract void onPageLoad(IPage page);
+
+    public abstract void onPageUnload(IPage page);
+}
