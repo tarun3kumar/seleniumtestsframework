8c5d5775dc978ad537d05fa036237e4313c288f8
tarun3kumar <tarun3kumar@yahoo.co.in>
2014-04-13 18:37:24 +0200

    add test steps

--- src/test/java/com/seleniumtests/tests/RetryTest2.java
+++ src/test/java/com/seleniumtests/tests/RetryTest2.java
@@ -6,6 +6,7 @@ import org.testng.annotations.Test;
 import static com.seleniumtests.core.CustomAssertion.assertThat;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.*;
 
 /**
  * Using Matchers
@@ -22,11 +23,10 @@ public class RetryTest2 extends SeleniumTestPlan {
         assertThat("2 is always equal to 2", 2, equalTo(2));   // This won't fail
         assertThat("2 is always equal to 2", 2, is(equalTo(2)));   // Same as previous statement
         assertThat("2 can not be equal to 3", 2, is(3));
-       /* assertThat("Hello", is(allOf(equalTo("Hello"), containsString("selenium"), startsWith("YouTube"), endsWith("Tellurium")))); // This fails
+        assertThat("Hello", is(allOf(equalTo("Hello"), containsString("selenium"), startsWith("YouTube"), endsWith("Tellurium")))); // This fails
         assertThat("Hello", is(not(allOf(equalTo("Hello"), containsString("selenium"), startsWith("YouTube"), endsWith("Tellurium"))))); // Negative condition hence succeeds
         assertThat("Hello", is(allOf(equalTo("Hello"), containsString("Hello"), startsWith("Hello"), endsWith("Hello"))));
         assertThat("Hello", is(anyOf(equalTo("Hello1"), is("Hello2"), containsString("ell1"), startsWith("1Hell"), endsWith("llo123")))); // This fails
-*/
     }
 
 }
