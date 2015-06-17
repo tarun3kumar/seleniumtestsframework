832ddc9ce45329ac658ea056e29a7d5194499507
tarun <tarun.kumar.bhadauria@zalando.de>
2015-06-17 12:01:06 +0200

    remove redundant parameters

--- src/main/java/com/seleniumtests/util/internal/entity/TestEntity.java
+++ src/main/java/com/seleniumtests/util/internal/entity/TestEntity.java
@@ -13,20 +13,13 @@
 
 package com.seleniumtests.util.internal.entity;
 
-import org.apache.commons.lang3.StringUtils;
-
 public class TestEntity {
-    public static final String TEST_CASE_ID = "TestEntity.TestCaseId";
     public static final String TEST_METHOD = "TestEntity.TestMethod";
-    public static final String TEST_TITLE = "TestEntity.TestTitle";
     public static final String TEST_DP_TAGS = "TestEntity.TestTags";
-    public static final String TEST_IS_ACTIVE = "TestEntity.IsActive";
 
     private String testCaseId = "";
     private String testMethod = "";
-    private String testTitle = "";
     private String testSite = "";
-    private boolean isActive = true;
 
     public String getTestCaseId() {
         return testCaseId;
@@ -38,18 +31,6 @@ public class TestEntity {
 
     public String getTestSite() {
         return testSite;
-    }
-
-    public String getTestTitle() {
-        return testTitle;
-    }
-
-    public boolean isActive() {
-        return isActive;
-    }
-
-    public void setActive(final boolean isActive) {
-        this.isActive = isActive;
     }
 
     public void setTestCaseId(final String testCaseId) {
@@ -64,26 +45,7 @@ public class TestEntity {
         this.testSite = testSite;
     }
 
-    public void setTestTitle(final String testTitle) {
-        this.testTitle = testTitle;
-    }
-
     public String toString() {
-        StringBuffer ret = new StringBuffer();
-        ret.append("Test Attributes: [ TestCaseId: " + testCaseId);
-        if (StringUtils.isNotEmpty(testMethod)) {
-            ret.append("|| TestMethod: " + testMethod);
-        }
-
-        if (StringUtils.isNotEmpty(testTitle)) {
-            ret.append("||TestTitle: " + testTitle);
-        }
-
-        if (!isActive) {
-            ret.append("||IsActive: " + isActive);
-        }
-
-        ret.append(" ]");
-        return ret.toString();
+        return ("Test Attributes: [ TestCaseId: " + testCaseId) + " ]";
     }
 }
