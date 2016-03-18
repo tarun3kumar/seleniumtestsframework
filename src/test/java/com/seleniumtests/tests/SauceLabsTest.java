fe99d4cac6c75d4fa39a8587b0d8367ec3e1ae6c
tarun <tkumarb@gmail.com>
2016-03-18 20:43:36 +0100

    add sauce lab test

---
+++ src/test/java/com/seleniumtests/tests/SauceLabsTest.java
@@ -0,0 +1,21 @@
+package com.seleniumtests.tests;
+
+
+import com.seleniumtests.core.SeleniumTestPlan;
+
+import com.seleniumtests.webpage.SauceLabsPage;
+
+import org.testng.annotations.Test;
+
+
+public class SauceLabsTest extends SeleniumTestPlan {
+
+    public SauceLabsTest() throws Exception {
+    }
+
+    @Test(groups = "sauce")
+    public void sauceLabsTest() throws Exception {
+        SauceLabsPage page = new SauceLabsPage(true);
+        page.getMeToTheSauceLabs();
+    }
+}
