b8692e7ecbab95895f55f82da81fbdfeda8948b4
Tarun Kumar <tarun.kumar@verimi.com>
2021-02-17 12:03:09 +0100

    Use WebDriver Manager

--- src/main/java/com/seleniumtests/browserfactory/ChromeCapabilitiesFactory.java
+++ src/main/java/com/seleniumtests/browserfactory/ChromeCapabilitiesFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 www.seleniumtests.com
+ * Copyright 2021 www.seleniumtests.com
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -13,9 +13,6 @@
 
 package com.seleniumtests.browserfactory;
 
-import java.io.File;
-import java.io.IOException;
-
 import org.openqa.selenium.Proxy;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.CapabilityType;
@@ -23,11 +20,6 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 import com.seleniumtests.driver.DriverConfig;
 import com.seleniumtests.driver.DriverMode;
-
-import com.seleniumtests.helper.FileUtility;
-import com.seleniumtests.helper.OSUtility;
-
-import com.seleniumtests.resources.WebDriverExternalResources;
 
 public class ChromeCapabilitiesFactory implements ICapabilitiesFactory {
 
@@ -72,48 +64,8 @@ public class ChromeCapabilitiesFactory implements ICapabilitiesFactory {
 
         // Set ChromeDriver for local mode
         if (webDriverConfig.getMode() == DriverMode.LOCAL) {
-            String chromeDriverPath = webDriverConfig.getChromeDriverPath();
-            if (chromeDriverPath == null) {
-                try {
-                    if (System.getenv("webdriver.chrome.driver") != null) {
-                        System.out.println("get Chrome driver from property:"
-                                + System.getenv("webdriver.chrome.driver"));
-                        System.setProperty("webdriver.chrome.driver", System.getenv("webdriver.chrome.driver"));
-                    } else {
-                        handleExtractResources();
-                    }
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                }
-            } else {
-                System.setProperty("webdriver.chrome.driver", chromeDriverPath);
-            }
         }
 
         return capability;
     }
-
-    public void handleExtractResources() throws IOException {
-        String dir = this.getClass().getResource("/").getPath();
-        dir = FileUtility.decodePath(dir);
-
-        if (!new File(dir).exists()) {
-            System.out.println("extracting chrome resources in " + dir);
-            FileUtility.extractJar(dir, WebDriverExternalResources.class);
-        }
-
-        if (!new File(dir + OSUtility.getSlash() + "chromedriver.exe").exists()) {
-            FileUtility.extractJar(dir, WebDriverExternalResources.class);
-        }
-
-        if (OSUtility.isWindows()) {
-            System.setProperty("webdriver.chrome.driver", dir + "\\chromedriver.exe");
-        } else if (OSUtility.isMac()) {
-            System.setProperty("webdriver.chrome.driver", dir + "/mac/chromedriver");
-        } else {
-            System.setProperty("webdriver.chrome.driver", dir + "/chromedriver");
-            new File(dir + "/chromedriver").setExecutable(true);
-        }
-    }
-
 }
